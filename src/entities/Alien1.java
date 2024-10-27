d6c6ab29a4eb711fe2694df5917e7cee7e951fee
IanF <gonzalo-francano@estudiante.mdp.utn.edu.ar>
2024-10-27 20:37:15 -0300

    Creation_EnemyManagerClass
    - New EnemyManager.
    - Implementation in Playing class.
    - Only Draw and Move
    - NO COLLISION (YET)

--- src/entities/Alien1.java
+++ src/entities/Alien1.java
@@ -6,6 +6,7 @@ public class Alien1 extends Enemy{
 
     // ====================> ATRIBUTOS <====================
 
+
     // ====================> CONTRUCTOR <====================
     public Alien1(float x, float y) {
         super(x, y, Alien_WIDTH, Alien_HEIGHT, Alien1);
