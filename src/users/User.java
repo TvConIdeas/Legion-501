dcd052662377695fb3537d29c408c2738e29b68b
delfi-fenoy <delfina-noel-fenoy-rivas@estudiante.mdp.utn.edu.ar>
2024-11-12 19:42:06 -0300

    addUserClass

---
+++ src/users/User.java
@@ -0,0 +1,25 @@
+package users;
+
+import main.Game;
+import utilz.LoadSave;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+import static utilz.Constants.EnemyConstants.*;
+
+public class User {
+
+    // ====================> ATRIBUTOS <====================
+    private String name;
+    
+
+    // ====================> CONTRUCTOR <====================
+
+    // ====================> GET | SET <====================
+
+    // ====================> METODOS <====================
+
+
+
+}
