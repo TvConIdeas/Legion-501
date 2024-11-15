a8ab2e7aeda181b6ca778db015e6b8992a640020
IanF <gonzalo-francano@estudiante.mdp.utn.edu.ar>
2024-11-15 05:58:42 -0300

    FinishRanking
    - Ranking is Complete

--- src/gameState/Ranking.java
+++ src/gameState/Ranking.java
@@ -1,17 +1,22 @@
 package gameState;
 
+import json.JSONUserManager;
 import main.Game;
 import ui.ClassButton;
+import users.User;
 import utilz.LoadSave;
 
 import java.awt.*;
+import java.awt.List;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
+import java.util.*;
 
 public class Ranking extends State implements Statemethods{
     // ====================> ATRIBUTOS <====================
     private ClassButton back;
+    private ArrayList<User> usuarios;
 
     // ====================> CONSTRUCTOR <====================
     public Ranking(Game game) {
@@ -22,8 +27,20 @@ public class Ranking extends State implements Statemethods{
     // ====================> GET | SET <====================
 
     // ====================> METODOS <====================
+    private void verifyRanking() {
+        boolean flag = false;
+        if(!flag){
+            JSONUserManager userManager = new JSONUserManager();
+            usuarios = new ArrayList<>(userManager.fileToUsers()); // Pasa el archivo a una lista
+            usuarios.sort((u1, u2) -> Integer.compare(u2.getBestScore(), u1.getBestScore()));
+            flag = true;
+        }
+    }
+
+
     @Override
     public void update() {
+        verifyRanking();
         back.update();
     }
 
@@ -34,6 +51,22 @@ public class Ranking extends State implements Statemethods{
         g.drawImage(pincel, 0, 0, Game.GAME_WIDTH, Game.GAME_HEIGHT, null);
 
         // Dibuja los nombres de los Ranking
+        g.setFont(new Font("Console", Font.BOLD, 20));
+        int y = 180;
+        for (int i = 0; i < usuarios.size() && i < 5; i++) {
+            User u = usuarios.get(i);
+            g.setColor(Color.WHITE);
+            g.drawString(u.getName(), 230, y);
+            g.setColor(Color.YELLOW);
+            g.drawString(String.valueOf(u.getBestScore()), 380, y);
+            y += 88;
+        }
+
+        // Dibuja la informacion del jugador
+        g.setColor(Color.WHITE);
+        g.drawString(game.getUserInGame().getName(), 200, 630);
+        g.setColor(Color.YELLOW);
+        g.drawString(String.valueOf(game.getUserInGame().getBestScore()), 200, 660);
 
         back.draw(g);
     }
